34c1bb4fb35687c730b7e669a07a670a0796bc24
lambertoo <rulindanal@gmail.com>
2023-09-10 12:03:34 -0700

    UNIX is basically a simple OS,but...

---
+++ 0x00-hello_world/101.quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print specified text without using
+ * function listed in man(3)printf and man(3)puts
+ *
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fprintf(stderr,
+			"and that piece of art is usefule\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
