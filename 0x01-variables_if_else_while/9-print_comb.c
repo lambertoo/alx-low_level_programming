0f6b6cff2b6b957e865a6f9f13ea7d719e7c2167
lambertoo <rulindanal@gmail.com>
2023-09-10 16:21:18 -0700

    rule forgotten

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,12 @@
  */
 int main(void)
 {
-	char n;
+	int n;
 
-	for (n = '0'; n <= '9'; n++)
+	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
-		if (n == '9')
+		if (n == 57)
 			continue;
 
 		putchar(',');
