94165df3b3aed0930c133bab6f2088d56332c4f9
lambertoo <rulindanal@gmail.com>
2023-09-10 10:31:46 -0700

    nahh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,15 @@
 /**
  * Main - Entry point
  *
- * Description: Prints on the std output
- *     by only using puts() func.
- *
- * Return: Always 0 (Sucess)
+ * Description:
+ * Prints on the std output by only using puts() func.
+ * Not allowed to use prinf() func
+ * End the program with 0 value.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
