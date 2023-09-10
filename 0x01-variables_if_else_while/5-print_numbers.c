4a6ba927ffa21523d71427d41cea21b9b991c3eb
lambertoo <rulindanal@gmail.com>
2023-09-10 15:29:59 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point never disappoint
+ *
+ * Description: Print all numbers
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+
+	printf("\n");
+
+	return (0);
+}
