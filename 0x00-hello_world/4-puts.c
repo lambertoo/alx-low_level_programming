be234cb049d45364ae2eaa3a506bbd91cefd8744
lambertoo <rulindanal@gmail.com>
2023-09-10 10:18:05 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Description: Prints on the std output
+ *     by only using puts() func.
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
