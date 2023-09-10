a77c4dd29502020faca4bce5c72312a9ea7d5c46
lambertoo <rulindanal@gmail.com>
2023-09-10 10:51:14 -0700

    hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - always the entry point
+ *
+ * Description: Print text using printf() fun.
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
