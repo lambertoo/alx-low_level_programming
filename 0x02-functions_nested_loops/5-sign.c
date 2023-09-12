965a472693c6004be694507a02cd2c5e3f4fe8ad
lambertoo <rulindanal@gmail.com>
2023-09-12 09:56:35 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "print_sign.h"
+#include "_putchar.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: character to verify
+ *
+ * Return: 1 if n greater than o
+ * -1 if n less than 0
+ * 0 otherwise
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/print_sign.h
@@ -0,0 +1,6 @@
+#ifndef _PRINT_SIGN_H_
+#define _PRINT_SIGN_H_
+
+int print_sign(int);
+
+#endif
