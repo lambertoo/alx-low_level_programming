2817a3edcef112015c76ee67cd8c129a9dbad0d9
lambertoo <rulindanal@gmail.com>
2023-09-12 13:23:56 -0700

    I'm federal agent Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+#include "jack_bauer.h"
+#include "_putchar.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * of jack bauer
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hr_d1, hr_d2;
+	int mn_d1, mn_d2;
+
+	for (hr_d1 = 0; hr_d1 < 3; hr_d1++)
+	{
+		for (hr_d2 = 0; hr_d2 <= 3; hr_d2++)
+		{
+			for (mn_d1 = 0; mn_d1 < 6; mn_d1++)
+			{
+				for (mn_d2 = 0; mn_d2 <= 9; mn_d2++)
+				{
+					_putchar(hr_d1 + 48);
+					_putchar(hr_d2 + 48);
+					_putchar(':');
+					_putchar(mn_d1 + 48);
+					_putchar(mn_d2 + 48);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/jack_bauer.h
@@ -0,0 +1,6 @@
+#ifndef _JACK_BAUER_H_
+#define _JACK_BAUER_H_
+
+void jack_bauer(void);
+
+#endif
