9873af1d49831039920fbdab94311ac134b65361
lambertoo <rulindanal@gmail.com>
2023-09-12 05:49:28 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "_putchar.h"
+#include "main.h"
+
+/**
+ * print_alphabet_10x - prints all alphabet 10 times
+ * in lowercase
+ *
+ * Return: (void)
+ */
+void print_alphabet_10x(void)
+{
+	char c;
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
