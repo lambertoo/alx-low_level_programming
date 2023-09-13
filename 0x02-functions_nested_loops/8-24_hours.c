4a4757a5871a786cf4491c4f4ee6746c9a29c79a
lambertoo <rulindanal@gmail.com>
2023-09-13 03:22:18 -0700

    24 chrono

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,8 +14,10 @@ void jack_bauer(void)
 
 	for (hr_d1 = 0; hr_d1 < 3; hr_d1++)
 	{
-		for (hr_d2 = 0; hr_d2 <= 3; hr_d2++)
+		for (hr_d2 = 0; hr_d2 <= 9; hr_d2++)
 		{
+			if  ((hr_d1 == 2) && (hr_d2 > 3))
+				break;
 			for (mn_d1 = 0; mn_d1 < 6; mn_d1++)
 			{
 				for (mn_d2 = 0; mn_d2 <= 9; mn_d2++)
