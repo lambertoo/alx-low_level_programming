2cea3dfc5f2aba2fec2a4c75a14a11190f0a5ae7
lambertoo <rulindanal@gmail.com>
2023-09-13 06:12:45 -0700

    learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,46 @@
+#include "times_table.h"
+#include "_putchar.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ * Description: nesting for loop
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int m;
+	int t;
+	int eq;
+	
+	for (m = 0; m <= 9; m++)
+	{
+		for (t = 0; t <= 9; t++)
+		{
+			eq = m * t;
+
+			if (eq < 10)
+			{
+				if ((t > 0) && (t <= 9))
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(eq + 48);
+			}
+			else if (eq > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((eq / 10) + 48);
+				_putchar((eq % 10) + 48);
+			}
+
+			if (t == 9)
+				continue;
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/times_table.h
@@ -0,0 +1,6 @@
+#ifndef _TIMES_TABLE_H_
+#define _TIMES_TABLE_H_
+
+void times_table(void);
+
+#endif
