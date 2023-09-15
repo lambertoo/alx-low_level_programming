063e174132c27a8af3f1681fe6ecfa668744ab24
lambertoo <rulindanal@gmail.com>
2023-09-14 20:51:53 -0700

    I feel like I am diagonally..

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "print_diagonal.h"
+#include "_putchar.h"
+
+/**
+ * print_diagonal - draw a diagonal line in terminal
+ * @n: length of the diagonal line to draw
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i = 1;
+
+	while (i <= n)
+	{
+		int j = 1;
+
+		++i;
+		while (j <= i)
+		{
+			if (i == 1)
+				continue;
+
+			_putchar(' ');
+			++j;
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+	if (n <= 0)
+		_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/print_diagonal.h
@@ -0,0 +1,6 @@
+#ifndef _PRINT_DIAGONAL_H_
+#define _PRINT_DIAGONAL_H_
+
+void print_diagonal(int);
+
+#endif
