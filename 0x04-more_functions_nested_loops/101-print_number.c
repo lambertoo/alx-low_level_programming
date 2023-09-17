90e8c13b5a4fde618b4974992fc2cc90c9af70af
lambertoo <rulindanal@gmail.com>
2023-09-17 11:16:59 -0700

    lets see

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -31,7 +31,7 @@ void positive_num(int n)
 	if (n >= 10000)
 	{
 		tnthsnd = (n / 10000) * 10000;
-		thsnd =  ((n - tnthsnd) / 1000) * 1000;
+		thsnd = ((n - tnthsnd) / 1000) * 1000;
 		hrd = ((n - thsnd) / 100) * 100;
 
 		_putchar((tnthsnd / 10000) + 48);
@@ -40,7 +40,7 @@ void positive_num(int n)
 		_putchar(((n - (thsnd + hrd)) / 10) + 48);
 		_putchar((n % 10) + 48);
 	}
-	if ((n >= 1000) && (n < 10000))
+	else if ((n >= 1000) && (n < 10000))
 	{
 		thsnd =  (n / 1000) * 1000;
 		hrd = ((n - thsnd) / 100) * 100;
@@ -94,13 +94,27 @@ void negative_num(int n)
 		_putchar(((posi - hrd) / 10) + 48);
 		_putchar((posi % 10) + 48);
 	}
-	else if (n <= -1000)
+	else if ((n <= -1000) && (n > -1000))
 	{
 		posi = n * (-1);
 		thsnd =  (posi / 1000) * 1000;
 		hrd = ((posi - thsnd) / 100) * 100;
 
 		_putchar('-');
+		_putchar((thsnd / 1000) + 48);
+		_putchar((hrd / 100) + 48);
+		_putchar(((posi - (thsnd + hrd)) / 10) + 48);
+		_putchar((posi % 10) + 48);
+	}
+	else if ((n <= -10000) && (n > -100000))
+	{
+		posi = n * (-1);
+		tnthsnd = (posi / 10000) * 10000;
+		thsnd = ((posi - tnthsnd) / 1000) * 1000;
+		hrd = ((posi - thsnd) / 100) * 100;
+
+		_putchar('-');
+		_putchar((tnthsnd / 10000) + 48);
 		_putchar((thsnd / 1000) + 48);
 		_putchar((hrd / 100) + 48);
 		_putchar(((posi - (thsnd + hrd)) / 10) + 48);
