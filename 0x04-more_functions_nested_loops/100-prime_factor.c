25292375d46ed6fd96832fbb6796661c54ebb151
lambertoo <rulindanal@gmail.com>
2023-09-17 09:15:12 -0700

    print gbnum

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,26 +9,37 @@ int main(void)
 {
 	long n = 612852475143;
 	int i;
+	long bgnum = 0;
 	
 	while (n % 2 == 0)
 		{
-			printf("%d ", 2);
+			/*printf("%d ", 2);*/
 			n = n / 2;
+			if (bgnum < 2)
+				bgnum = 2;
 		}
  
 	for (i = 3; i * i <= n; i = i + 2)
 		{
 			while (n % i == 0)
 				{
-					printf("%d ", i);
+					/*printf("%d ", i);*/
 					n = n / i;
+
+					if (bgnum < i)
+						bgnum = i;
 				}
 		}
 
 	if (n > 2)
-		printf("%ld ", n);
+	{
+		/*printf("%ld ", n);*/
 
-	printf("\n");
+		if (bgnum < n)
+			bgnum = n;
+	}
+	
+	printf("%ld \n", bgnum);
 
 	return (0);
 }
