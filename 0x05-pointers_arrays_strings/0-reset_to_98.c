8ad424efe42d14b3cd7b283c4ba2418fba20fbf0
lambertoo <rulindanal@gmail.com>
2023-09-19 03:46:51 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "reset_to_98.h"
+
+/**
+ * reset_to_98 - changes the value of variable address
+ * passed in as argument.
+ * @n: pointer variable
+ *
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/reset_to_98.h
@@ -0,0 +1,6 @@
+#ifndef RESET_TO_98_H
+#define RESET_TO_98_H
+
+void reset_to_98(int *n);
+
+#endif
