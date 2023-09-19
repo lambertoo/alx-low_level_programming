e04e4c9ae171f9ff92029e1322f6d2a843e6cc24
lambertoo <rulindanal@gmail.com>
2023-09-19 08:50:16 -0700

    this report, by its very length..

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include "_strlen.h"
+#include "_putchar.h"
+
+/**
+ * _strlen - counting characters in the string + '\0'
+ * returns it as the length of a string
+ * @s: string variable.
+ *
+ * Return: void.
+ */
+int _strlen(char *s)
+{
+	char *c;
+	int n;
+
+	c = s;
+	n = 0;
+	while (1)
+	{
+		if (c[n] == '\0')
+			break;
+		else
+			n++;
+	}
+
+	return (n + 1);
+}
---
+++ 0x05-pointers_arrays_strings/_strlen.h
@@ -0,0 +1,6 @@
+#ifndef _STRLEN_H
+#define _STRLEN_H
+
+int _strlen(char *s);
+
+#endif
