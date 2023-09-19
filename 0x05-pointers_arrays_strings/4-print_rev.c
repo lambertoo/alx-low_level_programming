b54e22770f7ca7d76adabab05b5f2f9101f8a2b7
lambertoo <rulindanal@gmail.com>
2023-09-19 09:34:51 -0700

    I can only go one way...

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "print_rev.h"
+#include "_putchar.h"
+
+/**
+ * print_rev - print a string in reverse end it with a new line
+ * @str: string to print in reverse
+ *
+ * Return: void.
+ */
+void print_rev(char *str)
+{
+	char *txt;
+	int n;
+
+	txt = str;
+	n = 0;
+	while (1)
+	{
+		if (txt[n] != '\0')
+			n++;
+		else
+			break;
+	}
+
+	for (; n >= 0; n--)
+		_putchar(txt[n]);
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/print_rev.h
@@ -0,0 +1,6 @@
+#ifndef PRINT_REV_H
+#define PRINT_REV_H
+
+void print_rev(char *s);
+
+#endif
