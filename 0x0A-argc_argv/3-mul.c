a296fef3454c76505ed50286a08672fd7d47e020
lambertoo <rulindanal@gmail.com>
2023-10-01 19:11:09 -0700

    fixation

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,14 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc > 1)
+	if (argc > 2)
 	{
 		printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+		return (0);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	return (0);
 }
