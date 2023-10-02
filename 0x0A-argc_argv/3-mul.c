9515871b4c5355cef6ba0dc86177191e92efa343
lambertoo <rulindanal@gmail.com>
2023-10-01 18:57:43 -0700

    Neither irony nor sarcasm is  argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: number of argument passed
+ * @argv: array of arguments passed
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", (atoi(argv[1]) * atoi(argv[2])));
+	(void)argc;
+
+	return (0);
+}
