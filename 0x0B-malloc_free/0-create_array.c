131baf47010856be766f95302a9104f81bb48eca
lambertoo <rulindanal@gmail.com>
2023-10-03 07:15:22 -0700

    that B betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 
 /**
  * create_array - creates an array of chars
- * @size: the size of the data 
+ * @size: the size of the data
  * @c: data to write
  *
  * Return: Pointer to array or NULL if it fails
@@ -12,10 +12,10 @@ char *create_array(unsigned int size, char c)
 {
 	char *charray;
 	unsigned int i;
-	
+
 	if (size == 0)
 		return (NULL);
-	
+
 	charray = (char *)malloc(sizeof(c) * size);
 
 	for (i = 0; i < size; i++)
