4e341af6a7c4741e708e0bde3103f9acb6fa86e5
lambertoo <rulindanal@gmail.com>
2023-10-04 02:34:49 -0700

    handled malloc & NULL

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,25 +12,24 @@
 char *_strdup(char *str)
 {
 	unsigned int len, i;
-	char *chrstr;
+	char *chrstr, *p;
 
-
+	if (str == NULL)
+		return ((char *) NULL);
 	len = 0, i = 0;
-
 	while (1)
 	{
 		len++;
 		if (str[len] == '\0')
 			break;
-
 	}
 	len++;
 
 	if (len == 0)
 		return ((char *) NULL);
 
-	chrstr = malloc(sizeof(char) * len);
-
+	p = malloc(sizeof(char) * len);
+	chrstr = p;
 	while (1)
 	{
 		if (str[i] == '\0')
@@ -39,6 +38,7 @@ char *_strdup(char *str)
 		chrstr[i] = str[i];
 		i++;
 	}
+	free(p);
 	chrstr[i++] = '\0';
 	return (chrstr);
 }
