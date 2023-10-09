502fac17811688a26572438ebaab1fb804224df1
lambertoo <rulindanal@gmail.com>
2023-10-09 13:42:28 -0700

    betty for struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure for dog object
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner fo the dog
+ */
 struct dog
 {
 	char *name;
