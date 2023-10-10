b3f307c2459ea1ad28efdcf9bf25b752e807221b
lambertoo <rulindanal@gmail.com>
2023-10-10 00:51:45 -0700

    outside of a dog, a book is a mans best friend..

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,14 @@ struct dog
 	char *owner;
 };
 
+/**
+ * typedef struct dog_t - Type for the dog object
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
