6339f56b0355806021ef2de358667599cd720c2c
lambertoo <rulindanal@gmail.com>
2023-10-24 15:26:33 -0700

    collecting output format

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,34 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list with a loop.
+ * print_listint_safe - Prints a listint_t linked list.
  * @head: Pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *hash_table[1024];
-	size_t i, count;
+	size_t count = 0;
+	const listint_t *current = head;
+	const listint_t *visited[1024];
+	size_t i;
 
-	for (i = 0; i < 1024; i++)
+	while (current)
 	{
-		hash_table[i] = NULL;
-	}
-	count = 0;
-	while (head)
-	{
-		if (hash_table[(unsigned long int)head % 1024] == head)
+		for (i = 0; i < count; i++)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			return (count);
+			if (current == visited[i])
+			{
+				printf("-> [%p] %d\n", (void *)current, current->n);
+				exit(98);
+			}
 		}
-		printf("[%p] %d\n", (void *)head, head->n);
-		hash_table[(unsigned long int)head % 1024] = head;
-		head = head->next;
+		visited[count] = current;
 		count++;
+		printf("[%p] %d\n", (void *)current, current->n);
+		current = current->next;
 	}
-	return (count);
+return (count);
 }
