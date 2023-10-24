f84de12129e7f10a304140ffab5c5eb333a505d6
lambertoo <rulindanal@gmail.com>
2023-10-24 06:02:54 -0700

    adding 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - Add a new node at the beginning of a listint_t list.
+ * @head: Pointer to a pointer to the first element of the list.
+ * @n: Integer to set in the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
+listint_t *add_nodeint(listint_t **head, int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 /**
- * struct listint_s - singly linked list
+ * struct listint_s - singly linked list for integers
  * @n: integer
  * @next: points to the next node
  *
@@ -16,13 +16,17 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, int n);
+size_t listint_len(const listint_t *h);
+
 /**
- * struct list_s - singly linked list
+ * struct list_s - singly linked list for strings
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node structure for strings
  */
 typedef struct list_s
 {
@@ -36,9 +40,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
 
 #endif
 
