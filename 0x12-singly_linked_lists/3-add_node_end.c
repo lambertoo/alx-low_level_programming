fad0fa5309cde2f2ca623024004c298cd9cfec82
lambertoo <rulindanal@gmail.com>
2023-10-24 15:19:54 -0700

    betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -7,35 +7,41 @@
  *
  * Return: new node created
  */
-list_t *add_node_end(list_t **head, const char *str) {
+list_t *add_node_end(list_t **head, const char *str)
+{
 	list_t *new_node;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        free(new_node);
-        return (NULL);
-    }
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->len = strlen(str);
-    new_node->next = NULL;
+	new_node->len = strlen(str);
+	new_node->next = NULL;
 
-    if (*head == NULL) {
-        *head = new_node;
-    } else {
-        list_t *current = *head;
-        while (current->next != NULL) {
-            current = current->next;
-        }
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		list_t *current = *head;
 
-        current->next = new_node;
-    }
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
 
-    return (new_node);
+	return (new_node);
 }
