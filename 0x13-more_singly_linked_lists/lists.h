5f2cfdb094c9545f16706e4077d71e113d7ee997
lambertoo <rulindanal@gmail.com>
2023-10-24 13:48:31 -0700

    adding 6-pop_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -41,6 +41,7 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
 /* Function prototypes for the new functions */
 listint_t *add_nodeint_end(listint_t **head, int n);
 void free_listint(listint_t *head);
