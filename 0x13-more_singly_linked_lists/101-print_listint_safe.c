d9e6a36c699cb3df6069bda0135bb319b28bcdfc
lambertoo <rulindanal@gmail.com>
2023-10-24 15:59:31 -0700

    101-print_listint_safe.c collection

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,10 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list.
- * @head: Pointer to the head of the list.
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: A pointer to the head of the linked list.
  *
  * Return: The number of nodes in the list.
  */
@@ -12,23 +12,23 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	const listint_t *current = head;
-	const listint_t *visited[1024];
-	size_t i;
+	const listint_t *check;
 
 	while (current)
 	{
-		for (i = 0; i < count; i++)
-		{
-			if (current == visited[i])
-			{
-				printf("-> [%p] %d\n", (void *)current, current->n);
-				exit(98);
-			}
-		}
-		visited[count] = current;
 		count++;
 		printf("[%p] %d\n", (void *)current, current->n);
+		check = head;
+		while (check != current)
+		{
+			if (check == current->next)
+			{
+				printf("-> [%p] %d\n", (void *)check, check->n);
+				return (count);
+			}
+			check = check->next;
+		}
 		current = current->next;
 	}
-return (count);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,44 +1,31 @@
 #include "lists.h"
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * free_listint_safe - Frees a listint_t list.
- * @h: Pointer to the head of the list.
+ * print_listint_safe - Prints a listint_t linked list.
+ * @head: Pointer to the head of the list.
  *
- * Return: The size of the list that was freed.
+ * Return: The number of nodes in the list.
  */
-size_t free_listint_safe(listint_t **h)
+size_t print_listint_safe(const listint_t *head)
 {
-	size_t size = 0;
-	listint_t *current, *tmp;
-	listint_t *visited[1024];
-	size_t i;
+    const listint_t *tortoise = head, *hare = head;
+    size_t count = 0;
 
-	if (!h || !(*h))
-	{
-		return (0);
-	}
-	current = *h;
-	while (current)
-	{
-		visited[size] = current;
-		size++;
-		if (size > 1024)
-		{
-			return (0);
-		}
-		tmp = current;
-		current = current->next;
-		free(tmp);
-		for (i = 0; i < size; i++)
-		{
-			if (current == visited[i])
-			{
-				*h = NULL;
-				return (size);
-			}
-		}
-	}
-	*h = NULL;
-	return (size);
+    while (hare && hare->next)
+    {
+        printf("[%p] %d\n", (void *)tortoise, tortoise->n);
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+        count++;
+
+        if (tortoise == hare)
+        {
+            printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
+            return count;
+        }
+    }
+
+    return count;
 }
+
