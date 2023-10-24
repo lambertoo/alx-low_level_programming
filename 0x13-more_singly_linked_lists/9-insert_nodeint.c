f77b04cd2dd79672d7a212b5df34e9676af31518
lambertoo <rulindanal@gmail.com>
2023-10-24 14:28:57 -0700

    solving betty warning

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position in a listint_t list.
+ * insert_nodeint_at_index - Inserts a new node at
+ * a given position in a listint_t list.
  * @head: Pointer to a pointer to the first element of the list.
  * @idx: Index at which to insert the new node.
  * @n: Integer to set in the new node.
