e609a64c4e379d312591637a091ea6ba1710c5d8
lambertoo <rulindanal@gmail.com>
2023-10-24 14:07:40 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -46,6 +46,6 @@ int pop_listint(listint_t **head);
 listint_t *add_nodeint_end(listint_t **head, int n);
 void free_listint(listint_t *head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
-
+int sum_listint(listint_t *head);
 #endif
 
